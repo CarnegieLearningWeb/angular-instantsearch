dda00b1a45143a0db9abf175a8c256a76cf31d59
Adetunji Oyebanji <tunji.oyebanji2015@gmail.com>
2025-02-05 16:27:23 +0000

    CURATE-584: Fixed typing issues

--- libs/angular-instantsearch/src/hierarchical-menu/hierarchical-menu-item.ts
+++ libs/angular-instantsearch/src/hierarchical-menu/hierarchical-menu-item.ts
@@ -20,7 +20,7 @@ import {
 
       <ul
         [class]="getListClass()"
-        *ngIf="item.isRefined && isArray(item.data) && item.data.length > 0"
+        *ngIf="item.isRefined && isArray(item.data) && (item.data || []).length > 0"
       >
         <ais-hierarchical-menu-item
           *ngFor="let child of item.data"
@@ -36,21 +36,21 @@ import {
     standalone: false
 })
 export class NgAisHierarchicalMenuItem {
-  @Input() public lvl: number = 1;
+  @Input() public lvl = 1;
   @Input() public refine: HierarchicalMenuRenderState['refine'];
   @Input() public createURL: HierarchicalMenuRenderState['createURL'];
   @Input() public item: HierarchicalMenuItem;
 
   public cx = bem('HierarchicalMenu');
 
-  public getItemClass(item): string {
+  public getItemClass(item: HierarchicalMenuItem): string {
     let className = this.cx('item');
 
     if (item.isRefined) {
       className = `${className} ${this.cx('item', 'selected')}`;
     }
 
-    if (this.isArray(item.data) && item.data.length > 0) {
+    if (this.isArray(item.data) && (item.data || []).length > 0) {
       className = `${className} ${this.cx('item', 'parent')}`;
     }
 
